351d278123d32f0c25c2c2ceabf115535c9be621
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2023-12-08 17:06:05 +0000

    Working in progress

--- chitter_challenge/app.py
+++ chitter_challenge/app.py
@@ -3,34 +3,13 @@ from flask import Flask, render_template, request, redirect, url_for
 from lib.database_connection import get_flask_database_connection
 from lib.user_repository import UserRepository
 from lib.user import User
-from flask_login import UserMixin
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
+from lib.forms import LoginForm, RegisterForm
 from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.config['SECRET_KEY'] = 'thisisasecretkey'
-
-class RegisterForm(FlaskForm):
-    username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-
-    password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
-    submit = SubmitField('Register')
-
-
-class LoginForm(FlaskForm):
-    username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-
-    password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
-    submit = SubmitField('Login')
 
 
 @app.route('/', methods=['GET'])
@@ -38,10 +17,13 @@ def main_page():
     form = LoginForm()
     return render_template('index.html', form=form)
 
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
-    form = LoginForm()
-    return render_template('login.html', form=form)
+    return render_template('login.html')
+
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
@@ -52,7 +34,7 @@ def register_page():
         hashed_password = bcrypt.generate_password_hash(form.password.data) 
         new_user = User(None, user_name=form.username.data, user_password=hashed_password)
         repository.create(new_user)
-        return redirect(url_for('login_page'))
+        return redirect(url_for('main_page'))
     return render_template('register.html', form=form)
 
 if __name__ == '__main__':
