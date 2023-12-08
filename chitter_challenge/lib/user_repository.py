dc19dc83e03d0fae2f2c46ba5b3b1dbcc8e3d96c
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2023-12-08 13:30:03 +0000

    Working on the tests

--- chitter_challenge/lib/user_repository.py
+++ chitter_challenge/lib/user_repository.py
@@ -4,6 +4,7 @@ class UserRepository:
     
     def __init__(self, connection):
         self._connection = connection
+        
     
     def all(self):
         rows = self._connection.execute('SELECT * from users')
@@ -12,3 +13,12 @@ class UserRepository:
             user = User(row['id'], row['user_name'], row['user_password'])
             users.append(user)
         return users
+    
+    def find(self, user_id):
+        row = self._connection.execute(
+            'SELECT * from users WHERE id = %s', [user_id])
+        
+        row = row[0]
+        return User(row['id'], row['user_name'], row['user_password'])
+        
+    
--- chitter_challenge/test/test_user_repository.py
+++ chitter_challenge/test/test_user_repository.py
@@ -12,3 +12,12 @@ def test_all_users(db_connection):
         User(1, 'Gustavo', 'Gustavo123'),
         User(2, 'Ashley', 'Ashley123')
     ]
+
+def test_find_single_user(db_connection):
+    db_connection.seed("seeds/users.sql")  # Seed our database with some test data
+    repository = UserRepository(db_connection)  # search for a single user 
+
+    user = repository.find(1)
+    print("HERE")
+    print(user)
+    assert user == User(1, "Gustavo", "Gustavo123")
