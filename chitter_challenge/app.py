be91a74c738a7a22e9edc6d576db87b0948d15ed
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2023-12-08 17:30:12 +0000

    Working in progress

--- chitter_challenge/app.py
+++ chitter_challenge/app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 from lib.database_connection import get_flask_database_connection
 from lib.user_repository import UserRepository
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from lib.user import User
 from lib.forms import LoginForm, RegisterForm
 from flask_bcrypt import Bcrypt
@@ -12,17 +13,32 @@ bcrypt = Bcrypt(app)
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view("login")
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/', methods=['GET'])
 def main_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        print("hello")
+        users = UserRepository.all()
+        for user in users:
+            if user.user_name == form.username.data:
+                if bcrypt.check_password_hash(user.user_password, form.password.data):
+                    login_user(user)
+                    return redirect(url_for('login_page'))
     return render_template('index.html', form=form)
 
 
-
 @app.route('/login', methods=['GET', 'POST'])
+@login_required
 def login_page():
     return render_template('login.html')
-
 
 
 @app.route('/register', methods=['GET', 'POST'])
--- chitter_challenge/lib/forms.py
+++ chitter_challenge/lib/forms.py
@@ -1,24 +1,14 @@
-from flask_login import UserMixin
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-
-    password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField('Register')
 
-
 class LoginForm(FlaskForm):
-    username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-
-    password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField('Login')
